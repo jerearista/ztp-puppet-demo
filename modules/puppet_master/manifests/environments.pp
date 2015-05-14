8be656d8c3919fbce65df5fa6e9627aeccf04155
Jere Julian <jere@arista.com>
2015-05-14 08:30:43 -0400

    Refactor environments class.  Getting files directly from githim instead of local files

--- modules/puppet_master/manifests/environments.pp
+++ modules/puppet_master/manifests/environments.pp
@@ -1,41 +1,38 @@
 # puppet_master/manifests/environments.pp
 #
-# Let Vagrant+puppet build the puppet master
+# Ensure Puppet is configured for directory-environments
 #
 class puppet_master::environments {
 
-require puppet_master
+  #require puppet_master
 
-$packages = [ "hiera",
-            ]
-package { $packages: ensure => installed, }
+  File {
+    require => Package['puppet-server', 'hiera'],
+  }
 
-file { "/etc/puppet/environments":
-  ensure => "link",
-  force  => true,
-  target => "/vagrant/files/puppet/environments",
-  require => Package['puppet-server'],
-}
-# Tell puppet to use directory environments
-augeas { "configure directory environments":
-  require => Package['puppet-server'],
-  context => "/files/etc/puppet/puppet.conf/main",
-  changes => [
-    'set environmentpath $confdir/environments',
-  ],
-  #onlyif => '/bin/grep environmentpath /etc/puppet/puppet.conf | /bin/grep -v "^\\s*environmentpath=\\$confdir/environments"',
-}
-file { "/etc/puppet/hiera.yaml":
-  ensure => "link",
-  force  => true,
-  target => "/vagrant/files/puppet/hiera.yaml",
-  require => Package['puppet-server', 'hiera'],
-} ->
-file { "/etc/hiera.yaml":
-  ensure => "link",
-  force  => true,
-  target => "/etc/puppet/hiera.yaml",
-  require => Package['puppet-server', 'hiera'],
-}
+  $packages = [ "hiera",
+              ]
+  package { $packages: ensure => installed, }
+
+  file { "/etc/puppet/environments":
+    ensure => directory,
+  }
+
+  # Tell puppet to use directory environments
+  augeas { "configure directory environments":
+    context => "/files/etc/puppet/puppet.conf/main",
+    changes => [
+      'set environmentpath $confdir/environments',
+    ],
+    #onlyif => '/bin/grep environmentpath /etc/puppet/puppet.conf | /bin/grep -v "^\\s*environmentpath=\\$confdir/environments"',
+    notify => Service['puppetmaster'],
+  }
+
+  file { "/etc/hiera.yaml":
+    ensure => "link",
+    force  => true,
+    target => "/etc/puppet/hiera.yaml",
+    require => Package['puppet-server', 'hiera'],
+  }
 
 }
