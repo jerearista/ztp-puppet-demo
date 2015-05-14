4faffe309142e5c3342bf0506cf6b5b46d7643c9
Jere Julian <jere@arista.com>
2015-05-14 08:22:06 -0400

    Detail puppet server definition and add default entry

--- manifests/default.pp
+++ manifests/default.pp
@@ -3,15 +3,23 @@
 #
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 
-include puppet_master
-#include puppet_master::passenger
-include puppet_master::environments
+node /^puppet\d+$/ {
+  include puppet_master::server_base
+  include puppet_master
+  #include puppet_master::passenger
+  include puppet_master::environments
+  include puppet_master::demo
 
-# Setup ztpserver dirs
-file { '/usr/share/ztpserver':
-  ensure => "link",
-  target => "/vagrant/files/ztpserver",
-  force  => true,
-  backup => true,
+  # Setup ztpserver dirs
+  file { '/usr/share/ztpserver':
+    ensure => "link",
+    target => "/vagrant/files/ztpserver",
+    force  => true,
+    backup => true,
+  }
+}
+
+node default {
+  notify { 'Hit default node classification.   Nothing to apply': }
 }
 
